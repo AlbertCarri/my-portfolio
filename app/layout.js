6b1485416fea85108af39ed42b746e9249626eb1
Alberto Carrizo <ae_carrizo@hotmail.com>
2025-09-12 17:23:30 -0300

    robots y sitemap no

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,15 @@
-import "./globals.css"
-import { Press_Start_2P } from "next/font/google"
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
-import React from "react"
+import "./globals.css";
+import { Press_Start_2P } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import React from "react";
 
-const pressStart2P = Press_Start_2P({ subsets: ["latin"], weight: "400" })
+const pressStart2P = Press_Start_2P({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
   title: "Retro Arcade Portfolio",
   description: "Portfolio profesional de Alberto Edelmiro Carrizo",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -17,8 +17,14 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta property="og:title" content="Mi Portfolio" />
-        <meta property="og:description" content="Mi página personal con proyectos" />
-        <meta property="og:image" content="https://edelbyte.com.ar/webog.webp" />
+        <meta
+          property="og:description"
+          content="Mi página personal con proyectos"
+        />
+        <meta
+          property="og:image"
+          content="https://edelbyte.com.ar/webog.webp"
+        />
         <meta property="og:type" content="website" />
       </head>
       <body className={pressStart2P.className}>
@@ -29,5 +35,5 @@ export default function RootLayout({ children }) {
         </div>
       </body>
     </html>
-  )
+  );
 }
